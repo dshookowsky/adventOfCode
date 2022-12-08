e1382bca23f833dcdcd7f9df518a4b128874fd0b
Dan Shookowsky <dshookowsky@gmail.com>
2022-12-08 14:09:37 -0500

    Day08

---
+++ 2022/Advent.Tests/Day08Tests.cs
@@ -0,0 +1,61 @@
+﻿using Day08;
+
+namespace Advent.Tests;
+
+[TestClass]
+public class Day08Tests
+{
+    [TestMethod]
+    public void Part1SolutionReturnsKnownValue()
+    {
+        var lines = new string[]
+        {
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390"
+        };
+
+        var solution = new Part1().Solution(lines);
+
+        Assert.AreEqual(21, solution);
+    }
+
+    [TestMethod]
+    public void Part2SolutionReturnsKnownValue()
+    {
+        var lines = new string[]
+        {
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390"
+        };
+
+        var solution = new Part2().Solution(lines);
+
+        Assert.AreEqual(8, solution);
+    }
+
+    [TestMethod]
+    public void Part1SolutionReturnsCorrectValue()
+    {
+        var lines = File.ReadAllLines(@"C:\Projects\AdventOfCode\2022\Day08\input.txt");
+
+        var part1 = new Part1();
+        var solution = part1.Solution(lines);
+        Assert.AreEqual(1695, solution);
+    }
+
+    [TestMethod]
+    public void Part2SolutionReturnsCorrectValue()
+    {
+        var lines = File.ReadAllLines(@"C:\Projects\AdventOfCode\2022\Day08\input.txt");
+
+        var part2 = new Part2();
+        var solution = part2.Solution(lines);
+        Assert.AreEqual(287040, solution);
+    }
+}
---
+++ 2022/Day08/Part1.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq.Expressions;
+
+namespace Day08;
+
+public class Part1
+{
+    private readonly List<int[]> m_forest = new();
+
+    public bool IsVisible(int row, int column, int xDirection, int yDirection, int height)
+    {
+        if (row == 0 || row == m_forest.Count -1 || column == 0 || column == m_forest[0].Length - 1)
+        {
+            return true;
+        }
+
+        if (height <= m_forest[row + xDirection][column + yDirection])
+        {
+            return false;
+        }
+
+        return IsVisible(row + xDirection, column + yDirection, xDirection, yDirection, height);
+    }
+
+    public int Solution(IEnumerable<string> lines)
+    {
+        foreach (var line in lines)
+        {
+            m_forest.Add(line.ToCharArray().Select(c => c - '0').ToArray());
+        }
+
+        int width = m_forest[0].Length;
+
+        int visibleTrees = 0;
+
+        for (int row = 0; row < m_forest.Count; row++)
+        {
+            for (int column = 0; column < width; column++)
+            {
+                int height = m_forest[row][column];
+                if (IsVisible(row, column, -1, 0, height) ||
+                    IsVisible(row, column, 1, 0, height)  ||
+                    IsVisible(row, column, 0, 1, height) ||
+                    IsVisible(row, column, 0, -1, height)) {
+                    visibleTrees++;
+                }
+            }
+        }
+
+        return visibleTrees;
+    }
+}
